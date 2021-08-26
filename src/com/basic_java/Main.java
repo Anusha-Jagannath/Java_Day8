0d38342ba049042fac5ef6584e13e63d140daca4
Anusha-Jagannath <67367819+Anusha-Jagannath@users.noreply.github.com>
2021-08-26 09:21:14 +0530

    Update Main.java

--- src/com/basic_java/Main.java
+++ src/com/basic_java/Main.java
@@ -3,8 +3,70 @@ package com.basic_java;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				//	CoinFlip coinflip=new CoinFlip();
+//		coinflip.calculate();
+//		
+//		LeapYear leap=new LeapYear();
+//		leap.findLeap();
+//		
+//		HarmonicNumber harmonic=new HarmonicNumber();
+//		harmonic.series();
+//
+//		
+//		Power2 power=new Power2();
+//		power.findPower(args[0]);
+//		
+//		PrimeFactors prime=new PrimeFactors();
+//		prime.primeFactor();
+//		
+		
+//		QuotientRemainder qr=new QuotientRemainder();
+//		qr.compute();
+		
+//		Swap swap=new Swap();
+//		swap.interchange();
+//		
+//		EvenOdd even=new EvenOdd();
+//		even.findEven();
+//		
+//		Vowel vowel=new Vowel();
+//		vowel.findVowel();
+//		
+//		Largest largest=new Largest();
+//		largest.findLarge();
+//		
+//		ArrayUtil array=new ArrayUtil();
+//		array.getInt2D();
+//		
+//		Triplets triplets=new Triplets();
+//		triplets.findTriplets();
+//		
+//		Distance distance=new Distance();
+//		distance.findDistance(args[0],args[1]);
+//
+//		Quadratic quadratic=new Quadratic();
+//		quadratic.findQuadratic();
+		
+//		WindChill wind=new WindChill();
+//		wind.findWind(args[0],args[1]);
+		
+//		FibonacciSeries fibonacci=new FibonacciSeries();
+//		fibonacci.printFibonacci();
+//		PerfectNumber perfect=new PerfectNumber();
+//		perfect.findPerfect();
+		
+//		PrimeNumber prime=new PrimeNumber();
+//		prime.isPrime();
+		
+//		ReverseNumber reverse=new ReverseNumber();
+//		reverse.findReverse();
+		
+//		Coupon coupon=new Coupon();
+//		coupon.collect();
+//		StopWatch stopwatch=new StopWatch();
+//		stopwatch.findTime();
+		VendingMachine vending = new VendingMachine();
+		vending.compute();
 	}
 
 }
